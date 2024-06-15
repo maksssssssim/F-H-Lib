a3da3f32f44674dba140585aed5beeb55581db76
Maks <128353018+maksssssssim@users.noreply.github.com>
2024-06-15 23:14:29 +0300

    Update scriptCharPage.js

--- scripts/scriptCharPage.js
+++ scripts/scriptCharPage.js
@@ -28,10 +28,16 @@ function tableInit(listData){
 function createUl(listData, id){
     let ul = document.createElement('ul');
 
+    console.log(listData);
+
     for(let i = 0; i < listData.lenght; i++){
+        console.log('цикл работает');
         let li = document.createElement('li');
+        console.log('элемент списка создается');
         li.innerHTML = `${listData[i]}`;
+        console.log('текст добавляется');
         ul.appendChild(li);
+        console.log('элемент добавляется в список');
     }
 
     document.getElementById(id).appendChild(ul);
