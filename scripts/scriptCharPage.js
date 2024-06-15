4da1255319b2608cffcd3a5ec61d5649c38ee16d
Maks <128353018+maksssssssim@users.noreply.github.com>
2024-06-15 22:23:45 +0300

    Update scriptCharPage.js

--- scripts/scriptCharPage.js
+++ scripts/scriptCharPage.js
@@ -290,6 +290,7 @@ function initApp(listName, char){
             </div>
             <div class="party_add" id='ul_4'>
                 <p class="h2_text">Эффекты и применение</p>
+                ${createUl(char.effects, 'ul_4')};
             </div>
         </div>`;
         aside.classList.add('item');
